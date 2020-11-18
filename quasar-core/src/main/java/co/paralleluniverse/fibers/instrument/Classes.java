70129d27732f89a392573883b54f3291b5cd2cb3
Paul Hatcher <paul.hatcher@r3.com>
2020-11-18 12:51:59 +0000

    CORE-104: Added support for Java FQDN for custom suspendable annotation

--- quasar-core/src/main/java/co/paralleluniverse/fibers/instrument/Classes.java
+++ quasar-core/src/main/java/co/paralleluniverse/fibers/instrument/Classes.java
@@ -21,6 +21,7 @@ import java.util.Set;
 import java.util.HashSet;
 import java.util.EnumMap;
 
+import co.paralleluniverse.common.resource.ClassLoaderUtil;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.MethodInsnNode;
 
@@ -88,10 +89,17 @@ final class Classes {
             return descIds.get(id).iterator().next();
         }
 
-        private void set(ID id, String s) { descIds.put(id, new HashSet<>(Arrays.asList(s))); }
+        private String toDescriptor(String s) {
+            // If string does not end in ';' then assume simple class name.
+            return ((s != null) && !s.isEmpty() && (s.charAt(s.length()-1) != ';'))
+                    ? "L" + ClassLoaderUtil.classToSlashed(s) + ";"
+                    : s;
+        }
+
+        private void set(ID id, String s) { descIds.put(id, new HashSet<>(Arrays.asList(toDescriptor(s)))); }
 
         private void add(ID id, String s) {
-            descIds.get(id).add(s);
+            descIds.get(id).add(toDescriptor(s));
         }
 
         boolean clear(String id) {
