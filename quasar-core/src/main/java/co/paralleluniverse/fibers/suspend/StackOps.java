107eb0b3fef353e95f27742755596a77b72bc98f
Chris Rankin <chris.rankin@r3.com>
2021-01-26 18:05:39 +0000

    Add copyright headers to StackOps and Stacks.

--- quasar-core/src/main/java/co/paralleluniverse/fibers/suspend/StackOps.java
+++ quasar-core/src/main/java/co/paralleluniverse/fibers/suspend/StackOps.java
@@ -1,3 +1,16 @@
+/*
+ * Quasar: lightweight threads and actors for the JVM.
+ * Copyright (c) 2021, R3 Ltd. All rights reserved.
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *
+ *   or (per the licensee's choosing)
+ *
+ * under the terms of the GNU Lesser General Public License version 3.0
+ * as published by the Free Software Foundation.
+ */
 package co.paralleluniverse.fibers.suspend;
 
 @SuppressWarnings("unused")
