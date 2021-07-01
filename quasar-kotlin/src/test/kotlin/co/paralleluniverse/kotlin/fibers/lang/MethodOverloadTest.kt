71d3bea2a0fd9369fc440dca5799adab2e3e4add
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2021-07-01 11:28:34 +0100

    CORE-1790: Use an inline function to make MethodOverloadTest more robust

--- quasar-kotlin/src/test/kotlin/co/paralleluniverse/kotlin/fibers/lang/MethodOverloadTest.kt
+++ quasar-kotlin/src/test/kotlin/co/paralleluniverse/kotlin/fibers/lang/MethodOverloadTest.kt
@@ -23,9 +23,14 @@ import org.junit.Test
 
 class MethodOverloadTest {
 
+    // This inline method helps reproduce the issue. Kotlin adds bytecode with a source line > the end of the source
+    // file
+    @Suspendable
+    inline fun reproduceTheIssueUsingInlineMethod() = arrayOf(0)
+
     @Suspendable
     fun function() {
-        function(arrayOf(0))
+        function(reproduceTheIssueUsingInlineMethod())
     }
 
     @Suspendable
