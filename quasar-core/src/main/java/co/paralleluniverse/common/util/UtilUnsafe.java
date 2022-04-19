ede2bc5aa913da98ebb729f674d851b0bc24efb9
Chris Rankin <chris.rankin@r3.com>
2022-04-19 10:39:21 +0100

    Simplify UtilUnsafe.

--- quasar-core/src/main/java/co/paralleluniverse/common/util/UtilUnsafe.java
+++ quasar-core/src/main/java/co/paralleluniverse/common/util/UtilUnsafe.java
@@ -13,7 +13,11 @@
 package co.paralleluniverse.common.util;
 
 import java.lang.reflect.Field;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
 import sun.misc.Unsafe;
+
+import static java.security.AccessController.doPrivileged;
 
 /**
  * Simple class to obtain access to the {@link Unsafe} object. {@link Unsafe}
@@ -27,46 +31,21 @@ public class UtilUnsafe {
     private UtilUnsafe() {
     }
 
-    public static Unsafe getUnsafe1() {
-        // Not on bootclasspath
-        if (UtilUnsafe.class.getClassLoader() == null)
-            return Unsafe.getUnsafe();
-        try {
-            Field f = Unsafe.class.getDeclaredField("theUnsafe");
-            f.setAccessible(true);
-            return (Unsafe) f.get(UtilUnsafe.class);
-        } catch (Exception e) {
-            throw new RuntimeException("Could not obtain access to sun.misc.Unsafe", e);
-        }
-    }
-
     public static Unsafe getUnsafe() {
         try {
             return Unsafe.getUnsafe();
         } catch (SecurityException se) {
             try {
-                return java.security.AccessController.doPrivileged(new java.security.PrivilegedExceptionAction<Unsafe>() {
+                return (Unsafe) doPrivileged(new PrivilegedExceptionAction<Object>() {
                     @Override
-                    public Unsafe run() throws Exception {
-                        final Class<sun.misc.Unsafe> k = sun.misc.Unsafe.class;
-                        if (true) {
-                            final Field f = k.getDeclaredField("theUnsafe");
-                            f.setAccessible(true);
-                            final Object x = f.get(null);
-                            return k.cast(x);
-                        } else {
-                            for (Field f : k.getDeclaredFields()) {
-                                f.setAccessible(true);
-                                final Object x = f.get(null);
-                                if (k.isInstance(x))
-                                    return k.cast(x);
-                            }
-                            throw new NoSuchFieldError("the Unsafe");
-                        }
+                    public Object run() throws Exception {
+                        final Field f = Unsafe.class.getDeclaredField("theUnsafe");
+                        f.setAccessible(true);
+                        return f.get(null);
                     }
                 });
-            } catch (java.security.PrivilegedActionException e) {
-                throw new RuntimeException("Could not initialize intrinsics", e.getCause());
+            } catch (PrivilegedActionException e) {
+                throw new SecurityException("Could not initialize intrinsics", e.getCause());
             }
         }
     }
